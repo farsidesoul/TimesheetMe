e76c28a6b86db11ee2344224ca28c51d2f079b0e
Darron Lumley <dlumley@iinet.net.au>
2015-05-31 08:07:01 +1000

    Added: Toolbar

--- app/src/main/java/au/com/bfbapps/timesheetme/LoginRegisterActivity.java
+++ app/src/main/java/au/com/bfbapps/timesheetme/LoginRegisterActivity.java
@@ -1,11 +1,11 @@
 package au.com.bfbapps.timesheetme;
 
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
-
-import au.com.bfbapps.timesheetme.adapters.LoginRegisterPagerAdapter;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 
 public class LoginRegisterActivity extends FragmentActivity {
 
-	private LoginRegisterPagerAdapter mLoginRegisterPagerAdapter;
 
 }
--- app/src/main/java/au/com/bfbapps/timesheetme/adapters/LoginRegisterPagerAdapter.java
+++ app/src/main/java/au/com/bfbapps/timesheetme/adapters/LoginRegisterPagerAdapter.java
@@ -1,8 +1,11 @@
 package au.com.bfbapps.timesheetme.adapters;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+
+import au.com.bfbapps.timesheetme.LoginFragment;
 
 public class LoginRegisterPagerAdapter extends FragmentPagerAdapter {
 	public LoginRegisterPagerAdapter(FragmentManager fm) {
@@ -11,11 +14,12 @@ public class LoginRegisterPagerAdapter extends FragmentPagerAdapter {
 
 	@Override
 	public Fragment getItem(int i) {
-		return null;
+		LoginFragment fragment = new LoginFragment();
+		return fragment;
 	}
 
 	@Override
 	public int getCount() {
-		return 0;
+		return 2;
 	}
 }
