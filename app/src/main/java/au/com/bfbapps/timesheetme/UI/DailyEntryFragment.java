b81896d38d72d43e2fe4d8cfb71e8a58f79f6f74
Darron Lumley <dlumley@iinet.net.au>
2015-06-02 20:32:55 +1000

    Enchanced: UI of Daily Entry fragment

--- app/src/main/java/au/com/bfbapps/timesheetme/UI/DailyEntryFragment.java
+++ app/src/main/java/au/com/bfbapps/timesheetme/UI/DailyEntryFragment.java
@@ -8,19 +8,27 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import java.util.zip.Inflater;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 import au.com.bfbapps.timesheetme.R;
 
 public class DailyEntryFragment extends Fragment {
 
+	private TextView mDateTextView;
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 	                         Bundle savedInstanceState) {
 		View v = inflater.inflate(R.layout.fragment_daily_entry, container, false);
 
+		Date date = new Date();
+		DateFormat dateFormat = new SimpleDateFormat("MMMM dd, yyyy", Locale.getDefault());
 
+		mDateTextView = (TextView)v.findViewById(R.id.daily_entry_date_field);
+		mDateTextView.setText(dateFormat.format(date));
 
 		return v;
 	}
