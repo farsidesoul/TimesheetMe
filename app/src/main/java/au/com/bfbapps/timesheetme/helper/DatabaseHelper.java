405734526f21d103a18e64b6eec8f1c93cd3b866
Darron Lumley <dlumley@iinet.net.au>
2015-06-04 18:57:15 +1000

    Added: CRUD Methods for all tables

--- app/src/main/java/au/com/bfbapps/timesheetme/helper/DatabaseHelper.java
+++ app/src/main/java/au/com/bfbapps/timesheetme/helper/DatabaseHelper.java
@@ -7,8 +7,13 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import au.com.bfbapps.timesheetme.Util.Dates;
 import au.com.bfbapps.timesheetme.models.Entry;
+import au.com.bfbapps.timesheetme.models.Job;
+import au.com.bfbapps.timesheetme.models.Task;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
 
@@ -22,7 +27,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     private static final String DATABASE_NAME = "timesheetMe";
 
     // Tables
-    private static final String TABLE_DAILY_ENTRY = "dailyEntry";
+    private static final String TABLE_ENTRY = "dailyEntry";
     private static final String TABLE_JOB = "job";
     private static final String TABLE_TASK = "task";
 
@@ -47,7 +52,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     // Table Creation
     // Daily Entry Table create
     private static final String CREATE_TABLE_DAILY_ENTRY = "CREATE TABLE "
-            + TABLE_DAILY_ENTRY + "(" + COLUMN_ID + " INTEGER PRIMARY KEY," + COLUMN_DATE
+            + TABLE_ENTRY + "(" + COLUMN_ID + " INTEGER PRIMARY KEY," + COLUMN_DATE
             + " DATE," + COLUMN_START_TIME + " DATETIME," + COLUMN_FINISH_TIME + " DATETIME,"
             + COLUMN_TOTAL_BREAK + " REAL," + COLUMN_TOTAL_HOURS_WORKED + " REAL,"
             + COLUMN_JOB_ID + " INTEGER," + COLUMN_TASK_ID + " INTEGER, FOREIGN KEY("
@@ -75,7 +80,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_DAILY_ENTRY);
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_ENTRY);
         sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_JOB);
         sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_TASK);
 
@@ -83,6 +88,8 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         onCreate(sqLiteDatabase);
     }
 
+
+	//region Entry CRUD
 	/**
 	 * Create entry in DB
 	 * @param entry Entry to be inserted into DB
@@ -101,33 +108,283 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 		values.put(COLUMN_TASK_ID, entry.getTaskId());
 
 		// insert row
-		long entryId = db.insert(TABLE_DAILY_ENTRY, null, values);
+		return db.insert(TABLE_ENTRY, null, values);
 
-		return entryId;
+
     }
 
 	/**
-	 * Fetching entries
+	 * Fetch a single entry
 	 */
 	public Entry getEntry(long entryId){
 		SQLiteDatabase db = this.getReadableDatabase();
 
-		String selectQuery = "SELECT * FROM " + TABLE_DAILY_ENTRY + " WHERE "
+		String selectQuery = "SELECT * FROM " + TABLE_ENTRY + " WHERE "
 				+ COLUMN_ID + " = " + entryId;
 
 		Log.e(TAG, selectQuery);
 
 		Cursor c = db.rawQuery(selectQuery, null);
 
-		if (c != null){
-			c.moveToFirst();
-		}
-
 		Entry entry = new Entry();
-		entry.setEntryId(c.getInt(c.getColumnIndex(COLUMN_ID)));
-		entry.setDate(Dates.ConvertStringToDate(c.getString(c.getColumnIndex(COLUMN_DATE))));
-		entry.setStart(Dates.ConvertStringToTime(c.getString(c.getColumnIndex(COLUMN_START_TIME))));
 
+		if (c != null) {
+			c.moveToFirst();
+
+			entry.setEntryId(c.getInt(c.getColumnIndex(COLUMN_ID)));
+			entry.setDate(Dates.ConvertStringToDate(c.getString(c.getColumnIndex(COLUMN_DATE))));
+			entry.setStart(Dates.ConvertStringToTime(c.getString(c.getColumnIndex(COLUMN_START_TIME))));
+			entry.setFinish(Dates.ConvertStringToTime(c.getString(c.getColumnIndex(COLUMN_FINISH_TIME))));
+			entry.setTotalBreak(c.getDouble(c.getColumnIndex(COLUMN_TOTAL_BREAK)));
+			entry.setTotalHoursWorked(c.getDouble(c.getColumnIndex(COLUMN_TOTAL_HOURS_WORKED)));
+			entry.setJobId(c.getInt(c.getColumnIndex(COLUMN_JOB_ID)));
+			entry.setTaskId(c.getInt(c.getColumnIndex(COLUMN_TASK_ID)));
+		}
 		return entry;
 	}
+
+	/**
+	 * Get all entries entered
+	 * @return ArrayList of entries
+	 */
+    public List<Entry> getAllEntries(){
+		List<Entry> entries = new ArrayList<>();
+		String selectQuery = "SELECT * FROM " + TABLE_ENTRY;
+
+		Log.e(TAG, selectQuery);
+
+		SQLiteDatabase db = this.getReadableDatabase();
+		Cursor c = db.rawQuery(selectQuery, null);
+
+		// Loop through the rows and add to the list
+		if (c.moveToFirst()){
+			do{
+				Entry entry = new Entry();
+				entry.setEntryId(c.getInt(c.getColumnIndex(COLUMN_ID)));
+				entry.setDate(Dates.ConvertStringToDate(c.getString(c.getColumnIndex(COLUMN_DATE))));
+				entry.setStart(Dates.ConvertStringToTime(c.getString(c.getColumnIndex(COLUMN_START_TIME))));
+				entry.setFinish(Dates.ConvertStringToTime(c.getString(c.getColumnIndex(COLUMN_FINISH_TIME))));
+				entry.setTotalBreak(c.getDouble(c.getColumnIndex(COLUMN_TOTAL_BREAK)));
+				entry.setTotalHoursWorked(c.getDouble(c.getColumnIndex(COLUMN_TOTAL_HOURS_WORKED)));
+				entry.setJobId(c.getInt(c.getColumnIndex(COLUMN_JOB_ID)));
+				entry.setTaskId(c.getInt(c.getColumnIndex(COLUMN_TASK_ID)));
+
+				entries.add(entry);
+
+			} while (c.moveToNext());
+		}
+
+		return entries;
+	}
+
+	/**
+	 * Get all Entries by a job id
+	 * @param jobId job Id to get entries for
+	 * @return list of entries
+	 */
+	public List<Entry> getAllEntriesByJobId(int jobId){
+		List<Entry> entries = new ArrayList<>();
+
+		String selectQuery = "SELECT * FROM " + TABLE_ENTRY + " te, "
+				+ TABLE_JOB + " tj, WHERE tj." + COLUMN_JOB_ID + " = te."
+				+ COLUMN_JOB_ID;
+
+		Log.e(TAG, selectQuery);
+
+		SQLiteDatabase db = this.getReadableDatabase();
+		Cursor c = db.rawQuery(selectQuery, null);
+
+		if(c.moveToFirst()){
+			do {
+				Entry entry = new Entry();
+				entry.setEntryId(c.getInt(c.getColumnIndex(COLUMN_ID)));
+				entry.setDate(Dates.ConvertStringToDate(c.getString(c.getColumnIndex(COLUMN_DATE))));
+				entry.setStart(Dates.ConvertStringToTime(c.getString(c.getColumnIndex(COLUMN_START_TIME))));
+				entry.setFinish(Dates.ConvertStringToTime(c.getString(c.getColumnIndex(COLUMN_FINISH_TIME))));
+				entry.setTotalBreak(c.getDouble(c.getColumnIndex(COLUMN_TOTAL_BREAK)));
+				entry.setTotalHoursWorked(c.getDouble(c.getColumnIndex(COLUMN_TOTAL_HOURS_WORKED)));
+				entry.setJobId(c.getInt(c.getColumnIndex(COLUMN_JOB_ID)));
+				entry.setTaskId(c.getInt(c.getColumnIndex(COLUMN_TASK_ID)));
+
+				entries.add(entry);
+			} while (c.moveToNext());
+		}
+		return entries;
+	}
+
+	/**
+	 * Update an entry with new values
+	 * @param entry entry to update
+	 * @return id of updated entry
+	 */
+	public int updateEntry(Entry entry){
+		SQLiteDatabase db = this.getWritableDatabase();
+
+		ContentValues values = new ContentValues();
+		values.put(COLUMN_DATE, entry.getDate().toString());
+		values.put(COLUMN_START_TIME, entry.getStart().toString());
+		values.put(COLUMN_FINISH_TIME, entry.getFinish().toString());
+		values.put(COLUMN_TOTAL_BREAK, entry.getTotalBreak());
+		values.put(COLUMN_TOTAL_HOURS_WORKED, entry.getTotalHoursWorked());
+		values.put(COLUMN_JOB_ID, entry.getJobId());
+		values.put(COLUMN_TASK_ID, entry.getTaskId());
+
+		return db.update(TABLE_ENTRY, values, COLUMN_ID + " = ?",
+				new String[] { String.valueOf(entry.getEntryId())});
+	}
+
+	/**
+	 * Delete entry by Id
+	 * @param entryId Id of entry to delete
+	 */
+	public void deleteEntry(long entryId){
+		SQLiteDatabase db = this.getWritableDatabase();
+		db.delete(TABLE_ENTRY, COLUMN_ID + " = ?",
+				new String[] { String.valueOf(entryId)});
+	}
+	//endregion
+
+	//region Task CRUD
+
+	/**
+	 * Create a new task
+	 * @param task task to be inserted
+	 * @return id of task inserted
+	 */
+	public long createTask(Task task){
+		SQLiteDatabase db = this.getWritableDatabase();
+
+		ContentValues values = new ContentValues();
+		values.put(COLUMN_ID, task.getTaskId());
+		values.put(COLUMN_TASK_NAME, task.getTaskName());
+
+		// insert row
+		return db.insert(TABLE_TASK, null, values);
+	}
+
+
+	/**
+	 * Get ArrayList of all tasks
+	 * @return ArrayList of tasks
+	 */
+	public List<Task> getAllTasks(){
+		List<Task> tasks = new ArrayList<>();
+		String selectQuery = "SELECT * FROM " + TABLE_TASK;
+
+		Log.e(TAG, selectQuery);
+
+		SQLiteDatabase db = this.getReadableDatabase();
+		Cursor c = db.rawQuery(selectQuery, null);
+
+		if (c.moveToFirst()) {
+			do {
+				Task task = new Task();
+				task.setTaskId(c.getInt(c.getColumnIndex(COLUMN_ID)));
+				task.setTaskName(c.getString(c.getColumnIndex(COLUMN_JOB_NAME)));
+
+				tasks.add(task);
+			} while (c.moveToNext());
+		}
+		c.close();
+		return tasks;
+	}
+
+
+	/**
+	 * Update task
+	 * @param task task details to update
+	 * @return id of task updated
+	 */
+	public int updateTask(Task task){
+		SQLiteDatabase db = this.getWritableDatabase();
+
+		ContentValues values = new ContentValues();
+		values.put(COLUMN_TASK_NAME, task.getTaskName());
+
+		return db.update(TABLE_TASK, values, COLUMN_ID + " = ?",
+				new String[] { String.valueOf(task.getTaskId())});
+	}
+
+	/**
+	 * Delete a task by id
+	 * @param taskId id of task to be deleted
+	 */
+	public void deleteTask(long taskId){
+		SQLiteDatabase db = this.getWritableDatabase();
+		db.delete(TABLE_TASK, COLUMN_ID + " = ?",
+				new String[]{String.valueOf(taskId)});
+	}
+
+	//endregion
+
+	//region Job CRUD
+
+	/**
+	 * Create a new job
+	 * @param job job to be inserted
+	 * @return id of job inserted
+	 */
+	public long createJob(Job job){
+		SQLiteDatabase db = this.getWritableDatabase();
+
+		ContentValues values = new ContentValues();
+		values.put(COLUMN_ID, job.getJobId());
+		values.put(COLUMN_JOB_NAME, job.getJobName());
+
+		// insert row
+		return db.insert(TABLE_JOB, null, values);
+	}
+
+
+	/**
+	 * Get ArrayList of all jobs
+	 * @return ArrayList of jobs
+	 */
+	public List<Job> getAllJobs(){
+		List<Job> jobs = new ArrayList<>();
+		String selectQuery = "SELECT * FROM " + TABLE_JOB;
+
+		Log.e(TAG, selectQuery);
+
+		SQLiteDatabase db = this.getReadableDatabase();
+		Cursor c = db.rawQuery(selectQuery, null);
+
+		if (c.moveToFirst()) {
+			do {
+				Job job = new Job();
+				job.setJobId(c.getInt(c.getColumnIndex(COLUMN_ID)));
+				job.setJobName(c.getString(c.getColumnIndex(COLUMN_JOB_NAME)));
+
+				jobs.add(job);
+			} while (c.moveToNext());
+		}
+		c.close();
+		return jobs;
+	}
+
+	/**
+	 * Update job
+	 * @param job Job details to update
+	 * @return id of job updated
+	 */
+	public int updateJob(Job job){
+		SQLiteDatabase db = this.getWritableDatabase();
+
+		ContentValues values = new ContentValues();
+		values.put(COLUMN_JOB_NAME, job.getJobName());
+
+		return db.update(TABLE_JOB, values, COLUMN_ID + " = ?",
+				new String[] { String.valueOf(job.getJobId())});
+	}
+
+	/**
+	 * Delete a job by id
+	 * @param jobId id of task to be deleted
+	 */
+	public void deleteJob(long jobId){
+		SQLiteDatabase db = this.getWritableDatabase();
+		db.delete(TABLE_JOB, COLUMN_ID + " = ?",
+				new String[]{String.valueOf(jobId)});
+	}
+	//endregion
 }
