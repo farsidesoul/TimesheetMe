8e081af6428d9bf7ff24c0673149a8cc258ccdd0
farsidesoul <farsidesoul@gmail.com>
2015-06-11 13:27:30 +1000

    Added: Workings of dynamically adding views to a layout

--- app/src/main/java/au/com/bfbapps/timesheetme/adapters/WeeklyEntryRecyclerViewAdapter.java
+++ app/src/main/java/au/com/bfbapps/timesheetme/adapters/WeeklyEntryRecyclerViewAdapter.java
@@ -1,10 +1,15 @@
 package au.com.bfbapps.timesheetme.adapters;
 
+import android.app.ActionBar;
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -44,20 +49,62 @@ public class WeeklyEntryRecyclerViewAdapter extends RecyclerView.Adapter<WeeklyE
 		mDb = new DatabaseHelper(mContext.getApplicationContext());
 
 		WeeklyEntry currentItem = mEntries.get(position);
-
 		holder.dayName.setText(currentItem.getDayOfWeek());
 		holder.dayDate.setText(Dates.ConvertDateToString(currentItem.getDayDate()));
 
-		List<String> jobList = new ArrayList<>();
+		// This works for setting the job layout.
+		// Inside this we need to create each view dynamically
+		// We can then put the views into the layout in the order required.
+		RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.MATCH_PARENT);
+		params.addRule(RelativeLayout.ALIGN_PARENT_END);
+
+		LinearLayout l = new LinearLayout(mContext);
+		l.setOrientation(LinearLayout.VERTICAL);
+
+
+		List<Job> jobList = new ArrayList<>();
 		List<Task> taskList = new ArrayList<>();
 
 		for (Entry entry : currentItem.getEntries()){
-			if (jobList.contains(entry.getJob().getJobName())){
+			if (jobList.contains(entry.getJob())){
+
 
 			} else {
-				jobList.add(entry.getJob().getJobName());
+				jobList.add(entry.getJob());
+//				TextView t = new TextView(mContext);
+//				t.setText(entry.getJob().getJobName());
+//				t.setTextColor(mContext.getResources().getColor(R.color.primaryColor));
 			}
+			RelativeLayout layout = new RelativeLayout(mContext);
+			TextView t = new TextView(mContext);
+			t.setText(entry.getJob().getJobName());
+			t.setTextColor(mContext.getResources().getColor(R.color.primaryColor));
+
+			TextView hours = new TextView(mContext);
+			hours.setText(entry.getTotalHoursWorked() + "");
+			hours.setTextColor(mContext.getResources().getColor(R.color.primaryColor));
+
+			layout.addView(t);
+			layout.addView(hours, params);
+
+			RelativeLayout layout2 = new RelativeLayout(mContext);
+			TextView task = new TextView(mContext);
+			task.setText(entry.getTask().getTaskName());
+			task.setTextColor(mContext.getResources().getColor(R.color.primaryColor));
+
+			TextView taskHours = new TextView(mContext);
+			taskHours.setText(entry.getTotalHoursWorked() + "");
+			taskHours.setTextColor(mContext.getResources().getColor(R.color.primaryColor));
+
+			layout2.addView(task);
+			layout2.addView(taskHours, params);
+
+			l.addView(layout);
+			l.addView(layout2);
 		}
+
+
+		holder.entryLayout.addView(l);
 	}
 
 	@Override
@@ -71,6 +118,7 @@ public class WeeklyEntryRecyclerViewAdapter extends RecyclerView.Adapter<WeeklyE
 
 	class WeeklyEntryItemViewHolder extends RecyclerView.ViewHolder {
 
+		LinearLayout entryLayout;
 		TextView dayName;
 		TextView dayDate;
 		TextView jobName;
@@ -80,6 +128,7 @@ public class WeeklyEntryRecyclerViewAdapter extends RecyclerView.Adapter<WeeklyE
 
 		public WeeklyEntryItemViewHolder(View itemView) {
 			super(itemView);
+			entryLayout = (LinearLayout)itemView.findViewById(R.id.weekly_entry_linear_layout);
 			dayName = (TextView)itemView.findViewById(R.id.weekly_entry_day_of_week);
 			dayDate = (TextView)itemView.findViewById(R.id.weekly_entry_date);
 			jobName = (TextView)itemView.findViewById(R.id.weekly_entry_job_name);
