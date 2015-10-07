67a137d86a62b051493782a41b5a7aec76d31fbc
Darron Lumley <farsidesoul@gmail.com>
2015-10-08 07:35:28 +1100

    Adjusted Colors

--- app/src/main/java/au/com/bfbapps/timesheetme/UI/ModeSelectActivity.java
+++ app/src/main/java/au/com/bfbapps/timesheetme/UI/ModeSelectActivity.java
@@ -4,28 +4,38 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import au.com.bfbapps.timesheetme.R;
 
-public class ModeSelectActivity extends AppCompatActivity {
-	
+public class ModeSelectActivity extends AppCompatActivity implements View.OnClickListener {
+
+	private CardView mSimpleMode;
+	private CardView mAdvancedMode;
+
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_mode_select);
-		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-		setSupportActionBar(toolbar);
-		
-		FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-		fab.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View view) {
-				Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-						.setAction("Action", null).show();
-			}
-		});
+
+		mSimpleMode = (CardView) findViewById(R.id.card_simple_mode);
+		mAdvancedMode = (CardView) findViewById(R.id.card_advanced_mode);
+		mSimpleMode.setOnClickListener(this);
+		mAdvancedMode.setOnClickListener(this);
+	}
+
+	public void onClick(View v){
+		if (v == mSimpleMode){
+			Toast.makeText(this, "Simple Mode Bro", Toast.LENGTH_SHORT).show();
+		} else if(v == mAdvancedMode){
+			Toast.makeText(this, "Advanced Mode Bro", Toast.LENGTH_SHORT).show();
+		}
 	}
 	
 }
