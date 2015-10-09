f249378927bdb4280479e45325ca370bfbbc8fa8
Darron Lumley <farsidesoul@gmail.com>
2015-10-09 17:21:37 +1100

    Changed color scheme Added logic for simple dialog fragment

--- app/src/main/java/au/com/bfbapps/timesheetme/ui/fragments/SimpleEntryDialogFragment.java
+++ app/src/main/java/au/com/bfbapps/timesheetme/ui/fragments/SimpleEntryDialogFragment.java
@@ -5,13 +5,11 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.FrameLayout;
 
 import au.com.bfbapps.timesheetme.R;
 
